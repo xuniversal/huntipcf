3bdc3f7dc1e3cc353ebdd84469ab5e22dc722eaf
nullvoid <77772721+xuniversal@users.noreply.github.com>
2025-02-19 16:38:33 +0700

    Update scan-proxy2.py

--- scan2/scan-proxy2.py
+++ scan2/scan-proxy2.py
@@ -30,7 +30,7 @@ def Save_to_file(filepath, data, cache):
 
 def Cek_ip_port(ip_address, port, save_path, active_cache, dead_cache):
     """Cek apakah proxy aktif atau tidak, lalu simpan hasilnya"""
-    result = f"{ip_address},{port},Unknown,Unknown"  # Tambahkan placeholder untuk country dan organization
+    result = f"{ip_address},{port}"
     
     if Cek_proxy(ip_address, port):
         Save_to_file(os.path.join(save_path, "active.txt"), result, active_cache)
@@ -53,16 +53,16 @@ def Read_ip_port(start_ip, end_ip, start_port, end_port, max_workers=100):
 
     # Menghasilkan rentang IP
     try:
-        # Menghasilkan semua IP dalam rentang yang diberikan
         ip_range = [str(ip) for ip in ipaddress.summarize_address_range(ipaddress.IPv4Address(start_ip), ipaddress.IPv4Address(end_ip))]
-
+        ip_list = []
+        for ip in ip_range:
+            ip_list.append(ip)
+        
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
             futures = []
-            for ip_block in ip_range:
-                # Untuk setiap IP dalam blok, kita perlu mengurai dan memindai port
-                for ip in ip_block:
-                    for port in range(start_port, end_port + 1):
-                        futures.append(executor.submit(Cek_ip_port, ip, port, save_path, active_cache, dead_cache))
+            for ip in ip_list:
+                for port in range(start_port, end_port + 1):
+                    futures.append(executor.submit(Cek_ip_port, ip, port, save_path, active_cache, dead_cache))
             for future in as_completed(futures):
                 future.result()  # Pastikan setiap tugas selesai
 
